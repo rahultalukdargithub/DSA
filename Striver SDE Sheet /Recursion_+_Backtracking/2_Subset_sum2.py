91153afb03ad6c8d43f319c84387abc604f5b8b7
Rahul Talukdar <rahultalukdar33714@gmail.com>
2026-08-06 21:39:55 +0530

    Clarify list copying in combination function
    Add comment to explain copying list 't' before appending.

--- Striver SDE Sheet /Recursion_+_Backtracking/2_Subset_sum2.py
+++ Striver SDE Sheet /Recursion_+_Backtracking/2_Subset_sum2.py
@@ -1,5 +1,6 @@
 class Solution:
     def comb(self,arr,l,t):
+        # if we do l.append(t) then the reference goes so next time when we are poping out the value will get disturbed so we have to send a copy of t 
         l.append(t[:])
         for i in range(len(arr)):
             if(i!=0 and arr[i]==arr[i-1]): 
